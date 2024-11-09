68f6c076d5cf408c565b960309c21c634095c81f
PRohithKumar-Reddy_finastra <PRohithKumar.Reddy@dh.com>
2024-11-09 12:05:21 +0530

    pagination and sort for API's

--- src/main/java/com/flipkart/ecommerce_backend/api/controllers/order/OrderController.java
+++ src/main/java/com/flipkart/ecommerce_backend/api/controllers/order/OrderController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.flipkart.ecommerce_backend.Constants.AppConstants;
 import com.flipkart.ecommerce_backend.models.LocalUser;
 import com.flipkart.ecommerce_backend.models.WebOrder;
 import com.flipkart.ecommerce_backend.services.OrderService;
@@ -15,13 +17,17 @@ import com.flipkart.ecommerce_backend.services.OrderService;
 @RestController
 @RequestMapping("api/orders")
 public class OrderController {
-	
+
 	@Autowired
 	private OrderService orderService;
-	
+
 	@GetMapping
-	public List<WebOrder> getOrders(@AuthenticationPrincipal LocalUser authenticationPrinciple){
-		return orderService.getOrders(authenticationPrinciple); 
+	public List<WebOrder> getOrders(@AuthenticationPrincipal LocalUser authenticationPrinciple,
+			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER,required=false) int pageNumber,
+			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE,required=false) int pageSize,
+			@RequestParam(value = "sort",defaultValue = "id",required=false) String sortBy,
+			@RequestParam(value = "sort_dir",defaultValue = AppConstants.SORT_DIR,required = false) String sortDir) {
+		return orderService.getOrders(authenticationPrinciple,pageNumber,pageSize,sortBy);
 	}
 
 }
--- src/main/java/com/flipkart/ecommerce_backend/api/controllers/product/ProductController.java
+++ src/main/java/com/flipkart/ecommerce_backend/api/controllers/product/ProductController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.flipkart.ecommerce_backend.Constants.AppConstants;
 import com.flipkart.ecommerce_backend.models.Product;
 import com.flipkart.ecommerce_backend.models.Repository.ProductRepository;
 import com.flipkart.ecommerce_backend.services.ProductService;
@@ -14,13 +17,25 @@ import com.flipkart.ecommerce_backend.services.ProductService;
 @RestController
 @RequestMapping("api/products")
 public class ProductController {
-	
+
 	@Autowired
 	private ProductService productService;
-	
+
 	@GetMapping
-	public List<Product> getProducts(){
-		return productService.getProducts();
+	public List<Product> getProducts(
+			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) int pageNumber,
+			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) int pageSize,
+			@RequestParam(value = "sortBy",defaultValue = "name",required = false) String sortBy,
+			@RequestParam(value = "sortDir",defaultValue = AppConstants.SORT_DIR,required = false) String sortDir) {
+//		pageNumber always starts from 0
+		return productService.getProducts(pageSize, pageNumber,sortBy,sortDir);
+	}	
+	
+	
+	@GetMapping("search/{keyword}")
+	public List<Product> searchProductByKeyword(@PathVariable String keyword){
+		System.out.println("akdf");
+		return productService.findByKeyword(keyword);
 	}
 
 }
