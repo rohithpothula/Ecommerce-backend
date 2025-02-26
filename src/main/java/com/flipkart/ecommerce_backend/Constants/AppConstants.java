ea77a63735a0d7eb3199916c21b019affd4504b9
bhanuprakash <prakashbhanu019@gmail.com>
2025-02-26 09:49:09 +0530

    fix formatting issues in AppConstants and included typo in variable declaration

--- src/main/java/com/flipkart/ecommerce_backend/Constants/AppConstants.java
+++ src/main/java/com/flipkart/ecommerce_backend/Constants/AppConstants.java
@@ -1,11 +1,10 @@
 package com.flipkart.ecommerce_backend.Constants;
 
 public class AppConstants {
-	
-	public static final String PAGE_NUMBER="0";
-	
-	public static final String PAGE_SIZE="10";
-	
-	public static final String SORT_DIR="ASC";
 
+  public static final Strin PAGE_NUMBER = "0";
+
+  public static final String PAGE_SIZE = "10";
+
+  public static final String SORT_DIR = "ASC";
 }
