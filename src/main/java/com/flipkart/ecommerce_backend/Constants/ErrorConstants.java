e00cf8dde0781d1c8742b5599d5a07e32bbf9fc7
bhanuprakash <prakashbhanu019@gmail.com>
2025-02-26 09:13:17 +0530

    fixed typo in ErrorConstants for INVALID_PASSWORD constant

--- src/main/java/com/flipkart/ecommerce_backend/Constants/ErrorConstants.java
+++ src/main/java/com/flipkart/ecommerce_backend/Constants/ErrorConstants.java
@@ -2,6 +2,6 @@ package com.flipkart.ecommerce_backend.Constants;
 
 public class ErrorConstants {
 
-	public static final Strin INVALID_PASSWORD = "INVALID_PASSWORD";
+	public static final String INVALID_PASSWORD = "INVALID_PASSWORD";
 
 }
