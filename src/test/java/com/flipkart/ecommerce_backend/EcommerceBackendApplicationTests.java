a43d12a4d72b222279473ab2b4f7d93181fde57c
bhanuprakash <prakashbhanu019@gmail.com>
2025-02-28 08:21:47 +0530

    refactor: update test method formatting and improve commit message regex length

--- src/test/java/com/flipkart/ecommerce_backend/EcommerceBackendApplicationTests.java
+++ src/test/java/com/flipkart/ecommerce_backend/EcommerceBackendApplicationTests.java
@@ -15,9 +15,10 @@ class EcommerceBackendApplicationTests {
 
   @Test
   public void repoTest() {
-              String className = this.localUserRepository.getClass().getName();
+    String className = this.localUserRepository.getClass().getName();
     String packageName = this.localUserRepository.getClass().getPackageName();
     // for now make test fail
-    //assert (className.equals("com.flipkart.ecommerce_backend.models.Repository.LocalUserRepository"));
+    // assert
+    // (className.equals("com.flipkart.ecommerce_backend.models.Repository.LocalUserRepository"));
   }
 }
